bfc7ca1f244faaeff3408595e1dcd29e427dfe03
Mohammed Salim <mohammed.23s@outlook.com>
2026-04-29 20:49:54 +0400

    The frontend directory has been updated

---
+++ frontend/src/app/employee-details/employee-details.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-employee-details',
+  imports: [],
+  templateUrl: './employee-details.html',
+  styleUrl: './employee-details.css',
+})
+export class EmployeeDetails {}
---
+++ frontend/src/app/update-employee/update-employee.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-update-employee',
+  imports: [],
+  templateUrl: './update-employee.html',
+  styleUrl: './update-employee.css',
+})
+export class UpdateEmployee {}
